256e75ce4b4676820552df3c37ad08bb65dad1a8
Robani-G <robanigirma2021@gmail.com>
2023-10-24 19:06:09 +0300

    video

--- src/app/Donate/page.tsx
+++ src/app/Donate/page.tsx
@@ -6,7 +6,7 @@ function Donate() {
     <main className="min-h-screen  flex justify-center items-center sm:flex-row flex-col gap-4 w-full ">
       <Form />
       <div>
-        <video className="w-full sm:max-w-[42rem]   p-2">
+        <video className="w-full sm:max-w-[42rem]   p-2" loop muted autoPlay>
           <source src="/video.mp4" />
         </video>
         <p className=" text-2xl  max-w-[50rem] text-gray-500 p-2  ">
