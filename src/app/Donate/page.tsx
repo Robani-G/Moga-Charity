1f282dbc9cc6a3644917bfd288348b5fdc727fde
Robani-G <robanigirma2021@gmail.com>
2023-10-25 11:49:29 +0300

    video size fixed

--- src/app/Donate/page.tsx
+++ src/app/Donate/page.tsx
@@ -6,10 +6,10 @@ function Donate() {
     <main className="min-h-screen  flex justify-center items-center sm:flex-row flex-col gap-4 w-full ">
       <Form />
       <div>
-        <video className="w-full sm:max-w-[42rem]   p-2" loop muted autoPlay>
+        <video className="w-full sm:max-w-[46rem]   p-2" loop muted autoPlay>
           <source src="/video.mp4" />
         </video>
-        <p className=" text-2xl  max-w-[50rem] text-gray-500 p-2  ">
+        <p className=" text-2xl  max-w-[46rem] text-gray-500 p-2  ">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
           asperiores sed suscipit. Placeat praesentium nisi cum voluptatibus
           dolores corrupti, recusandae maxime aliquid aspernatur ?
