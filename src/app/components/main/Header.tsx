ce76a4014379d8ac2b2a6ef2ce8a34d623f9c301
Robani-G <robanigirma2021@gmail.com>
2023-10-25 11:51:44 +0300

    video size fixed

--- src/app/components/main/Header.tsx
+++ src/app/components/main/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="h-12  mt-2 p-2 flex items-center justify-between">
+    <div className="h-12  mt-2 p-2 flex fixed  items-center justify-between">
       <a href="/" className="font-semibold">
         Moga
       </a>
